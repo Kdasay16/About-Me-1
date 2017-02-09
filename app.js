2e23220d6df54f5052fe5ddd2d42703c5c374480
Kurt Maurer <kurtrm@gmail.com>
2017-02-08 17:04:17 -0800

    Corrected some minor errors with the quiz.

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ var quizResponseOne = prompt('Please answer the following questions with Y or N.
 if(quizResponseOne.toUpperCase() === 'Y' || quizResponseOne.toUpperCase() === 'YES'){
   console.log('Yikes! Actually, Kurt lives in Fremont.');
   document.write('Does Kurt live in Wallingford? Answer: Yikes! Actually, Kurt lives in Fremont.');
-} else if (quizResponseOne.toUpperCase() !== 'N' || quizResponseOne.toUpperCase() !== 'NO') {
-  console.log('Please answer Y or N for now on.');
-} else {
+} else if (quizResponseOne.toUpperCase() === 'N' || quizResponseOne.toUpperCase() === 'NO') {
   console.log('Correct!');
   document.write('Does Kurt live in Wallingford? Answer: Correct!');
+} else {
+  console.log('Please answer Y or N for now on.');
 }
 
 var quizResponseTwo = prompt('Was Kurt a submariner?');
@@ -24,11 +24,11 @@ var quizResponseTwo = prompt('Was Kurt a submariner?');
 if(quizResponseTwo.toUpperCase() === 'Y' || quizResponseTwo.toUpperCase() === 'YES'){
   console.log('Correct!');
   document.write('Was Kurt a submariner? Answer: Correct!');
-} else if (quizResponseTwo.toUpperCase() !== 'N' || quizResponseTwo.toUpperCase() !== 'NO') {
-  console.log('Please answer Y or N for now on.');
-} else {
+} else if (quizResponseTwo.toUpperCase() === 'N' || quizResponseTwo.toUpperCase() === 'NO') {
   console.log('Incorrect. Kurt was a submariner on the USS Kentucky.');
   document.write('Was Kurt a submariner? Answer: Incorrect. Kurt was a submariner on the USS Kentucky.');
+} else {
+  console.log('Please answer Y or N for now on.');
 }
 
 var quizResponseThree = prompt('Did Kurt see a lot when looking out of the windows on the submarine?');
@@ -36,11 +36,11 @@ var quizResponseThree = prompt('Did Kurt see a lot when looking out of the windo
 if(quizResponseThree.toUpperCase() === 'N' || quizResponseThree.toUpperCase() === 'NO'){
   console.log('Correct! Kurt said there aren\'t any windows on submarines!');
   document.write('Did Kurt see a lot when looking out of the windows on the submarine? Answer: Correct! Kurt said there aren\'t any windows on submarines!');
-} else if (quizResponseThree.toUpperCase() !== 'N' || quizResponseThree.toUpperCase() !== 'NO') {
-  console.log('Please answer Y or N for now on.');
-} else {
+} else if (quizResponseThree.toUpperCase() === 'Y' || quizResponseThree.toUpperCase() === 'YES') {
   console.log('Oh no! Remember, there are no windows on submarines.');
   document.write('Did Kurt see a lot when looking out of the windows on the submarine? Answer: Oh no! Remember, there are no windows on submarines.');
+} else {
+  console.log('Please answer Y or N for now on.');
 }
 
 var quizResponseFour = prompt('Did Kurt get to shoot any missiles from his submarine?');
@@ -48,11 +48,11 @@ var quizResponseFour = prompt('Did Kurt get to shoot any missiles from his subma
 if(quizResponseFour.toUpperCase() === 'Y' || quizResponseFour.toUpperCase() === 'YES'){
   console.log('Fuck yea he did!');
   document.write('Did Kurt get to shoot any missiles from his submarine? Answer: Fuck yea he did!');
-} else if (quizResponseFour.toUpperCase() !== 'N' || quizResponseFour.toUpperCase() !== 'NO') {
-  console.log('Please answer Y or N for now on.');
-} else {
+} else if (quizResponseFour.toUpperCase() === 'N' || quizResponseFour.toUpperCase() === 'NO') {
   console.log('Actually, Kurt got to shoot two test missiles while he was on board.');
   document.write('Did Kurt get to shoot any missiles from his submarine? Answer: Actually, Kurt got to shoot two test missiles while he was on board.');
+} else {
+  console.log('Please answer Y or N for now on.');
 }
 
 var quizResponseFive = prompt('Is Kurt from Seattle?');
@@ -60,11 +60,11 @@ var quizResponseFive = prompt('Is Kurt from Seattle?');
 if(quizResponseFive.toUpperCase() === 'N' || quizResponseFive.toUpperCase() === 'NO'){
   console.log('Correct!');
   document.write('Is Kurt from Seattle? Answer: Correct!');
-} else if (quizResponseFive.toUpperCase() !== 'N' || quizResponseFive.toUpperCase() !== 'NO') {
-  console.log('Please answer Y or N for now on.');
-} else {
+} else if (quizResponseFive.toUpperCase() === 'Y' || quizResponseFive.toUpperCase() === 'YES') {
   console.log('He loves Seattle but grew up in Auburn, AL.');
   document.write('Is Kurt from Seattle? Answer: He loves Seattle but grew up in Auburn, AL.');
+} else {
+  console.log('Please answer Y or N for now on.');
 }
 
 alert('Thanks for playing!');
