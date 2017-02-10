4c0bad08dc642a6e41fae8a8988f09a508bb1f11
Kurt Maurer <kurtrm@gmail.com>
2017-02-10 12:12:09 -0800

    Initial branch commit. Fixed the fucking for loop.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var userName = prompt('What is your name?');
 var confirmQuiz = confirm('Welcome! Would you like to take a quiz about Kurt?');
 
+//CONSIDER ADDING CONSOLELOGS TO PROVIDE FEEDBACK TO DEVELOPER.
+
 if(confirmQuiz){
   console.log('Great! Let\'s get started!');
 } else {
@@ -10,15 +12,19 @@ if(confirmQuiz){
 
 //User input stored in array.
 var quizResponses = new Array (
-prompt('Please answer the following questions with Y or N. Does Kurt live in Wallingford?'),
-prompt('Was Kurt a submariner?'),
-prompt('Did Kurt see a lot when looking out of the windows on the submarine?'),
-prompt('Did Kurt get to shoot any missiles from his submarine?'),
-prompt('Is Kurt from Seattle?')
+'Please answer the following questions with Y or N. Does Kurt live in Wallingford?',
+'Was Kurt a submariner?',
+'Did Kurt see a lot when looking out of the windows on the submarine?',
+'Did Kurt get to shoot any missiles from his submarine?',
+'Is Kurt from Seattle?'
 );
+//^^TAKE QUESTIONS AND MAKE THEM VARIABLES. REMOVE PROMPT.
 
-//All correct responses to the user's input.
+//INSERT A LIST OF CORRECT ANSWER VARIABLES HERE. THEN INSERT THOSE ANSWERS INTO AN ARRAY.
+
+//All correct FEEDBACK to the user's input.
 var correctAnswers = new Array (
+  //^^CHANGE TO FEEDBACK AND MAKE EACH ONE A VARIABLE
 'Fremont is awesome.',
 'Kurt was on the USS Kentucky.',
 'Kurt said there aren\'t any windows on submarines!',
@@ -28,6 +34,7 @@ var correctAnswers = new Array (
 
 //All incorrect responses to the user's input.
 var incorrectAnswers = new Array (
+  //^^CHANGE TO FEEDBACK. MAKE EACH FEEDBACK STATEMENT A VARIABLE
 'Yikes! Actually, Kurt lives in Fremont.',
 'Actually, Kurt was a submariner on the USS Kentucky.',
 'Oh no! Remember, there are no windows on submarines.',
@@ -39,8 +46,14 @@ var answerCount = 0;
 
 //Loop for the quiz. This is not very robust, as I can't figure out how to interrupt the for loop for an invalid response.
 for (var i = 0; i < quizResponses.length; i++) {
-  //If yes, then log either the correct or incorrect answer to the console and write it to the HTML doc.
-  if (quizResponses[i].toUpperCase() === 'Y' || quizResponses[i].toUpperCase() === 'YES') {
+  flag = false;
+/*Lesson learned: Previously, I put the prompts inside of the quizResponses array. I
+also attempted to put the prompt at the beginning of the for loop. What the loop would end up doing
+is getting the values of each prompt, storing them in the array, then checking the statements. The code below
+will store the input into the variable, then continue the script correctly.*/
+  var newResponse = prompt(quizResponses[i]);
+  if (newResponse.toUpperCase() === 'Y' || newResponse.toUpperCase() === 'YES') {
+    flag = true;
     if (i === 0 || i === 2 || i === 4) {
       console.log(incorrectAnswers[i]);
       document.write('<ul><li> Incorrect. ' + incorrectAnswers[i] + '</li></ul>');
@@ -50,7 +63,8 @@ for (var i = 0; i < quizResponses.length; i++) {
       answerCount++;
     }
     //If no, then log either the correct or incorrect answer to the console and write it to the HTML doc.
-  } else if (quizResponses[i].toUpperCase() === 'N' || quizResponses[i].toUpperCase() === 'NO') {
+  } else if (newResponse.toUpperCase() === 'N' || newResponse.toUpperCase() === 'NO') {
+    flag = true;
     if (i === 1 || i === 3) {
       console.log(incorrectAnswers[i]);
       document.write('<ul><li> Incorrect. ' + incorrectAnswers[i] + '</li></ul>');
@@ -59,12 +73,17 @@ for (var i = 0; i < quizResponses.length; i++) {
       document.write('<ul><li> Correct. ' + correctAnswers[i] + '</li></ul>');
       answerCount++;
     }
+  } else if (flag === false) {
+    alert('I AM A ROBOT. INVALID RESPONSE.');
+    i--;
   }
 };
 //Write the results of the quiz to the console.
 document.write('<p>You got ' + answerCount + '/5 correct!</p>');
 
 //If the user got all five questions correct, then they can do two bonus questions.
+
+//ADD CONSOLE FEEDBACK TO SEE THE RANDOM NUMBER THAT WAS GENERATED.
 if (answerCount === 5) {
   alert('Good job! You get two bonus questions!');
 
@@ -89,13 +108,15 @@ if (answerCount === 5) {
     }
   }
 };
+//^^^^^WRITE RESULTS TO WEBSITE AND WHAT THE NUMBER WAS.
 
+//CORRECT STATEMENT ABOUT GETTING TWO BONUS QUESTIONS.
 //If they got the first bonus question correct, the user can attempt the last bonus question.
 if (answerCount === 6) {
   alert('You\'ve answered a shit ton of answers correctly. Here is the last bonus question.');
   alert('Guess a state Kurt has lived in. You get six guesses.');
 
-  var statesLivedIn = ['alabama', 'florida', 'south carolina', 'washington', 'new york', 'connecticut'];
+  var statesLivedIn = ['Alabama', 'Florida', 'South Carolina', 'Washington', 'New York', 'Connecticut'];
   var statesGuessesLeft = 5;
   var maxGuesses = 6;
   var statesComplete = false;
@@ -103,10 +124,10 @@ if (answerCount === 6) {
   for (var statesCount = 0; statesCount < maxGuesses; statesCount++) {
     var statesQuestion = prompt('Kurt has lived in: ');
     for (i = 0; i < statesLivedIn.length; i++) {
-      if (statesQuestion.toLowerCase() === statesLivedIn[i]) {
+      if (statesQuestion.toLowerCase() === statesLivedIn[i].toLowerCase()) {
         statesComplete = true;
         statesLivedIn.splice(i, 1);
-        alert('You guessed correctly! He\'s also lived in ' + statesLivedIn.toString() + '.');
+        alert('You guessed correctly! He\'s also lived in ' + statesLivedIn.join(', ') + '.');
         document.write('<p>You got both bonus questions correct!</p>');
         break;
       }
@@ -116,11 +137,16 @@ if (answerCount === 6) {
     } else {
       alert('He has never lived in that state. You have ' + statesGuessesLeft-- + ' guesses remaining.');
       if (statesCount === maxGuesses - 1) {
-        alert('You\'re out of guesses! He has lived in ' + statesLivedIn.toString() + '.');
+        alert('You\'re out of guesses! He has lived in ' + statesLivedIn.join(', ') + '.');
         document.write('<p>You got one bonus question correct!</p>');
       }
     }
   }
 }
+//^^^^WRITE RESULTS TO THE WEBPAGE
 
 document.write('Thanks for playing ' + userName + '!');
+
+//ENDING NOTES: NEED MORE DEVELOPER FEEDBACK IN THE CONSOLE.
+//CONTROLLED ENTRY INTO THE WEBPAGE. RESULTS OF A QUESTION IS IMMEDIATELY LOGGED. BUT THIS MAY HAVE MORE TO DO WITH THE PROPERTIES OF PROMPTS.
+//
